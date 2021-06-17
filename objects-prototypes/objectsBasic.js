b10344ba5bd188decadbe1f274a81ac09a6ac299
Debasis Saikia <debasishsaikia136@gmail.com>
2021-06-17 23:01:37 +0530

    first object

--- objects-prototypes/objectsBasic.js
+++ objects-prototypes/objectsBasic.js
@@ -1,3 +1,6 @@
-let obj = {};
-
-console.log(obj);
+const person = {
+  firstName: "Debasis",
+  age: 24,
+  lastName: "Saikia",
+};
+console.log(person["firstName"] + " is " + person.age + " years old");
