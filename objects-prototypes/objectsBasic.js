48ccb02f4e178cd1afe57c7863e1d036d2d3ad90
Debasis Saikia <debasishsaikia136@gmail.com>
2021-06-17 23:15:19 +0530

    delete and in operator

--- objects-prototypes/objectsBasic.js
+++ objects-prototypes/objectsBasic.js
@@ -3,4 +3,15 @@ const person = {
   age: 24,
   lastName: "Saikia",
 };
+
+// adding new properties
+person.job = "Developer";
+// object modification
+person.firstName = "Dev";
+// delete properties
+delete person.lastName;
+
+// checking existing properties
+console.log("tall" in person);
+
 console.log(person["firstName"] + " is " + person.age + " years old");
