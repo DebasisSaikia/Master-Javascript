484bd26cf970b2825df2be60b456bc7a395ab4b2
Debasis Saikia <debasishsaikia136@gmail.com>
2021-06-30 22:06:02 +0530

    recursive function '

--- functions/funcBasics.js
+++ functions/funcBasics.js
@@ -17,3 +17,35 @@ function add() {
   console.log(arguments[1]);
 }
 add(1, 2, 3);
+
+function greet(name) {
+  console.log(`hello ${name}`)
+}
+
+let name = prompt('enter your name')
+
+greet(name)
+
+const addNum = function (a, b) {
+  return a + b
+}
+const result = addNum(1, 4)
+console.log(result)
+
+// recursive function
+// program to count down numbers to 1
+function countDown(number) {
+
+  // display the number
+  console.log(number);
+
+  // decrease the number value
+  const newNumber = number - 1;
+
+  // base case
+  if (newNumber > 0) {
+    countDown(newNumber);
+  }
+}
+
+countDown(4);
