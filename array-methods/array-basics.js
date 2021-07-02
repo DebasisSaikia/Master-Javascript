4e2afc3b6fccfac1717a903bbe8b1e3959b89566
Debasis Saikia <debasishsaikia136@gmail.com>
2021-07-02 23:06:13 +0530

    basic method

---
+++ array-methods/array-basics.js
@@ -0,0 +1,16 @@
+const lang = ['js', 'python', 'go'];
+
+// find length of array
+// console.log(lang.length)
+
+// adding to last
+// lang.push('dart')
+// adding to first
+// lang.unshift('dart')
+
+// removing from last
+// lang.pop()
+// removing first
+lang.shift()
+
+console.log(lang)
