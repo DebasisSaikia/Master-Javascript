320371d1a6ebf69877ab130f335b89f8a20f9683
Debasis Saikia <debasishsaikia136@gmail.com>
2021-07-20 23:12:10 +0530

    function practice

--- functions/funcBasics.js
+++ functions/funcBasics.js
@@ -1,51 +1,76 @@
-// function declaration
-// function dec() {}
+// // function declaration
+// // function dec() {}
 
-// function expression
-// const login = function () {
-//   console.log("function expression");
-// };
-// login();
+// // function expression
+// // const login = function () {
+// //   console.log("function expression");
+// // };
+// // login();
 
-// IIFE-immediately invoked function expression
-(function login() {
-  console.log("IIFE");
-})();
+// // IIFE-immediately invoked function expression
+// (function login() {
+//   console.log("IIFE");
+// })();
 
-// arguments objects
-function add() {
-  console.log(arguments[1]);
-}
-add(1, 2, 3);
+// // arguments objects
+// function add() {
+//   console.log(arguments[1]);
+// }
+// add(1, 2, 3);
 
-function greet(name) {
-  console.log(`hello ${name}`)
-}
+// function greet(name) {
+//   console.log(`hello ${name}`)
+// }
 
-let name = prompt('enter your name')
+// let name = prompt('enter your name')
 
-greet(name)
+// greet(name)
 
-const addNum = function (a, b) {
-  return a + b
-}
-const result = addNum(1, 4)
-console.log(result)
+// const addNum = function (a, b) {
+//   return a + b
+// }
+// const result = addNum(1, 4)
+// console.log(result)
 
-// recursive function
-// program to count down numbers to 1
-function countDown(number) {
+// // recursive function
+// // program to count down numbers to 1
+// function countDown(number) {
 
-  // display the number
-  console.log(number);
+//   // display the number
+//   console.log(number);
 
-  // decrease the number value
-  const newNumber = number - 1;
+//   // decrease the number value
+//   const newNumber = number - 1;
 
-  // base case
-  if (newNumber > 0) {
-    countDown(newNumber);
-  }
-}
+//   // base case
+//   if (newNumber > 0) {
+//     countDown(newNumber);
+//   }
+// }
 
-countDown(4);
+// countDown(4);
+
+
+// var i = 3;
+// function change(j) {
+//   j = 4;
+// }
+// change(i);
+// console.log(i)
+
+// MyClass = function () {
+//   this.a = 'hello';
+//   this.b = 'world'
+// }
+// MyClass.prototype.a = 'john';
+// MyClass.prototype.c = 12
+// obj = new MyClass()
+
+// console.log(obj.c)
+
+var a = {}
+var b = a
+a.v = 1;
+b.v = 2
+
+console.log(a.v)
