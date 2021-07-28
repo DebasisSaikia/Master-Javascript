7c3b408af2ef2cd2240059d3f5bf58b9bf6162c5
Debasis Saikia <debasishsaikia136@gmail.com>
2021-07-28 21:17:20 +0530

    closure advance

--- closures/clBasic.js
+++ closures/clBasic.js
@@ -12,3 +12,49 @@ const outer = () => {
     inner()
 }
 outer()
+
+// lexical scope
+// function foo() {
+//     var memo = 'i am crazy';
+
+//     return function dit() {
+//         console.log(memo)
+//     }
+// }
+// const result = foo()
+// result()
+
+const myFunction = () => {
+    let myValue = 2;
+    console.log(myValue);
+
+    const childFunction = () => {
+        console.log(myValue += 1);
+    }
+
+    return childFunction;
+}
+
+const result = myFunction();
+console.log(result);
+result();
+result();
+result();
+
+
+// closure practice
+const parent = () => {
+    var name = 'debasis';
+
+    const child = () => {
+        console.log(name)
+    }
+
+    return child
+}
+/*function parent is closed here but the inner function will still have access to the variable of outer function with its lexical
+scope. this is how beautiful javascript function .thats why we call javascript function as a first class citizen
+*/
+const output = parent()
+console.log(output)
+output()
