8acc96c50adad6d570d77f6d7629bd6bd844e228
Debasis Saikia <debasishsaikia136@gmail.com>
2021-07-29 23:45:11 +0530

    file changed

--- closures/moreClosure.js
+++ closures/moreClosure.js
@@ -1,45 +1,73 @@
 
-// closure practice
-// function outer() {
-//     // var a = 10
-//     function inner() {
-//         console.log(a, b)
+// // closure practice
+// // function outer() {
+// //     // var a = 10
+// //     function inner() {
+// //         console.log(a, b)
+// //     }
+// //     let a = 100
+// //     return inner
+// // }
+// // const result = outer()
+// // result()
+
+
+// // complex example
+
+// function parent() {
+//     var c = 10;
+//     function child1(b) {
+//         let a = 20
+//         function child2() {
+//             console.log(a, b, c)
+//         }
+//         return child2
 //     }
-//     let a = 100
-//     return inner
+//     return child1
 // }
-// const result = outer()
+// var result = parent()('debasis')
 // result()
 
 
-// complex example
+// // data hiding with closure
+// // function counter() {
+// //     var count = 0;
+// //     function innerCount() {
+// //         count++
+// //         console.log(count)
+// //     }
+// //     return innerCount;
+// // }
 
-function parent() {
-    var c = 10;
-    function child1(b) {
-        let a = 20
-        function child2() {
-            console.log(a, b, c)
-        }
-        return child2
-    }
-    return child1
-}
-var result = parent()('debasis')
-result()
+// // var res = counter()
+// // res(); res(); res()
+
+// // var res2 = counter()()
 
 
-// data hiding with closure
-function counter() {
-    var count = 0;
-    function innerCount() {
-        count++
-        console.log(count)
-    }
-    return innerCount;
-}
+// // counter with constructor function
+// function Counter() {
+//     var a = 0
+//     this.increment = () => {
+//         a++
+//         console.log(a)
+//     }
+//     this.decrement = () => {
+//         a--
+//         console.log(a)
+//     }
+// }
+// var counter1 = new Counter()
 
-var res = counter()
-res(); res(); res()
+// counter1.increment()
+// counter1.increment()
+// counter1.decrement()
 
-var res2 = counter()()
+// counter1.decrement()
+// counter1.decrement()
+
+// const a = (function () {
+//     return parseInt("1.5")
+// })()
+// a()
+
