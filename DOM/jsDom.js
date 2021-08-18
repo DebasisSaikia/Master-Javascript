fd6efab8153d877b4142720392c4bc385d7e615a
Debasis Saikia <debasishsaikia136@gmail.com>
2021-08-18 13:29:27 +0530

    querySelector

--- DOM/jsDom.js
+++ DOM/jsDom.js
@@ -1,4 +1,34 @@
 console.log('JS DOM');
 
-const title = document.getElementById('title');
+/**
+ * getElementById
+ * const title = document.getElementById('title');
 console.log(title)
+ */
+
+/**
+ * getElementsByTagName
+ * const heading = document.getElementsByTagName('h1')
+console.log(heading[0])
+ */
+
+/**
+ *
+ * querySelector
+ * const query = document.querySelector('#title');
+console.log(query)
+ */
+
+/**
+ * querySelector methods
+ * const parent = document.querySelector('.parent');
+console.log(parent.childNodes)
+ */
+
+
+/**
+ * 
+ */
+const query = document.querySelector('.children');
+console.log(query.nextElementSibling)
+
