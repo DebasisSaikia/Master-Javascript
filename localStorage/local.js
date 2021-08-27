7148bdc2c760cf1b0dc3ad7587a7530cc15ea4a3
Debasis Saikia <debasishsaikia136@gmail.com>
2021-08-27 17:40:21 +0530

    Json parse

--- localStorage/local.js
+++ localStorage/local.js
@@ -4,4 +4,22 @@ const person = {
   age: 24,
 };
 
-localStorage.setItem("theme", JSON.stringify(person.name));
+const butn = document.getElementById("btn");
+const setItem = document.getElementById("set");
+const getItem = document.getElementById("get");
+
+butn.addEventListener("click", clearLocal);
+setItem.addEventListener("click", setLocal);
+getItem.addEventListener("click", getLocal);
+
+function clearLocal() {
+  localStorage.removeItem("theme");
+}
+function setLocal() {
+  localStorage.setItem("theme", JSON.stringify(person));
+}
+
+function getLocal() {
+  const item = JSON.parse(localStorage.getItem("theme"));
+  console.log(item?.name || "no set");
+}
