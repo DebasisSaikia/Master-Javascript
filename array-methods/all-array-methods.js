b3cca6265550e6f2a7db84683270990cefe1b3e3
Debasis Saikia <debasishsaikia136@gmail.com>
2021-09-01 21:02:42 +0530

    filter array

--- array-methods/all-array-methods.js
+++ array-methods/all-array-methods.js
@@ -1,25 +1,32 @@
 // multidimensional array
-let arr1 = ['deba', 24];
-let arr2 = ['ilish', 23];
+let arr1 = ["deba", 24];
+let arr2 = ["ilish", 23];
 let arr = [arr1, arr2];
 // console.log(arr[1][1]) //first element and first index
 // addding to multidimensional array
 // arr[1].push('biki')
 // console.log(arr)
 // splice to add and element o specific index
-arr.splice(1, 0, ['biki'])
-console.log(arr)
+arr.splice(1, 0, ["biki"]);
+console.log(arr);
 
 // join method
-const fruit=['apple','guava','banana','grapes'];
-const res=fruit.join('-')
-console.log(res)
+const fruit = ["apple", "guava", "banana", "grapes"];
+const res = fruit.join("-");
+console.log(res);
 
 // splice method
-const sp=fruit.splice(2,'pineapple')
-console.log(sp)
+const sp = fruit.splice(2, "pineapple");
+console.log(sp);
 
 // map
-const numbers=[5,1,3,2,6];
-const output=numbers.map((x)=>x.toExponential(2));
-console.log(output);
+const numbers = [5, 1, 3, 2, 6];
+const output = numbers.map((x) => x.toString(2));
+// console.log(output);
+
+// filter array
+const fil = [5, 1, 3, 2, 6,7,4,9];
+
+const filData = fil.filter((odd) => odd % 2);
+const evenData=fil.filter((even)=>even%2==0)
+console.log(evenData)
