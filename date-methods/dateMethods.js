1ea51a35747d76456636cdd939e9647b70f31aca
Debasis Saikia <debasishsaikia136@gmail.com>
2021-09-26 16:11:35 +0530

    date method

---
+++ date-methods/dateMethods.js
@@ -0,0 +1,7 @@
+const today = new Date();
+console.log(today);
+console.log(today.getFullYear());
+console.log(today.setFullYear(2015));
+console.log(today.toDateString());
+console.log(today.toLocaleString());
+console.log(today.toString());
