5492bf57708cbe3ad36d4b0b0d4fde6c6511b15d
Debasis Saikia <debasishsaikia136@gmail.com>
2021-11-07 20:40:57 +0530

    found index of array

--- js-array-practice/basic.js
+++ js-array-practice/basic.js
@@ -15,5 +15,40 @@ function arrayMaker(n) {
     return Array.prototype.slice.call(arguments);
   }
 }
-var b=arrayMaker({7:1},{4:5});
-console.log(b)
+var b = arrayMaker({ 7: 1 }, { 4: 5 });
+console.log(b);
+
+// ==================================================COPY array in JavaScript
+// method 1 -slice method
+const slc = [1, 2, 3, 4, 5];
+const res = slc.slice();
+const spr = [...slc, 5, 5, 5];
+console.log(spr);
+
+// ==================================================CHECK if an array
+const check = [1, 2, 3, 4, 5];
+const ch = Array.isArray(check);
+console.log(ch);
+// method 2
+function isarr(object) {
+  if (typeof object === "string") {
+    console.log("its array");
+  } else {
+    console.log("its not array");
+  }
+}
+var ary=[1,2,3]
+isarr(ary)
+
+// =================check the index and value
+const cc=['1','2','4','5','7'];
+console.log(cc.indexOf('4'))
+
+function checkIn(item){
+    if(cc.indexOf(item)===-1){
+        console.log('not found')
+    }else{
+        console.log('we found it')
+    }
+}
+checkIn('7');
